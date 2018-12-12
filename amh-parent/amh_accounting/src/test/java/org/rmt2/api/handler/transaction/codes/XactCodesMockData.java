5d7db33b89f6f47beed4a4828a45bae3f93f0d51
rmt2bsc <rmt2bsc@gmail.com>
2018-12-12 12:22:56 -0600

    IS-23:  Completed the development of Transaction Code Message handler.

--- amh-parent/amh_accounting/src/test/java/org/rmt2/api/handler/transaction/codes/XactCodesMockData.java
+++ amh-parent/amh_accounting/src/test/java/org/rmt2/api/handler/transaction/codes/XactCodesMockData.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.dao.mapping.orm.rmt2.XactCodeGroup;
+import org.dao.mapping.orm.rmt2.XactCodes;
+import org.dto.XactCodeDto;
 import org.dto.XactCodeGroupDto;
 import org.dto.adapter.orm.transaction.Rmt2XactDtoFactory;
 import org.rmt2.api.AccountingMockDataFactory;
@@ -42,5 +44,32 @@ public class XactCodesMockData extends SubsidiaryMockData {
         list.add(d);
         return list;
     }
-    
+ 
+    /**
+     * 
+     * @return
+     */
+    public static final List<XactCodeDto> createMockXactCode() {
+        List<XactCodeDto> list = new ArrayList<>();
+        XactCodes o = AccountingMockDataFactory.createMockOrmXactCode(201, 101, "Code 1");
+        XactCodeDto d = Rmt2XactDtoFactory.createXactCodeInstance(o);
+        list.add(d);
+        
+        o = AccountingMockDataFactory.createMockOrmXactCode(202, 102, "Code 2");
+        d = Rmt2XactDtoFactory.createXactCodeInstance(o);
+        list.add(d);
+        
+        o = AccountingMockDataFactory.createMockOrmXactCode(203, 103, "Code 3");
+        d = Rmt2XactDtoFactory.createXactCodeInstance(o);
+        list.add(d);
+        
+        o = AccountingMockDataFactory.createMockOrmXactCode(204, 104, "Code 4");
+        d = Rmt2XactDtoFactory.createXactCodeInstance(o);
+        list.add(d);
+        
+        o = AccountingMockDataFactory.createMockOrmXactCode(205, 105, "Code 5");
+        d = Rmt2XactDtoFactory.createXactCodeInstance(o);
+        list.add(d);
+        return list;
+    }
 }
