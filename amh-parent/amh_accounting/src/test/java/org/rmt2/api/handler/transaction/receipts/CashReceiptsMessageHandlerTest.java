c7a1284ce1e0f23cec2662b0df015723581f5443
appdev <royterrell2@gmail.com>
2019-11-01 14:59:30 -0500

    IS-27: Created

---
+++ amh-parent/amh_accounting/src/test/java/org/rmt2/api/handler/transaction/receipts/CashReceiptsMessageHandlerTest.java
@@ -0,0 +1,203 @@
+package org.rmt2.api.handler.transaction.receipts;
+
+import static org.mockito.Matchers.isA;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+
+import org.dto.XactDto;
+import org.dto.XactTypeDto;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.modules.transaction.XactApiException;
+import org.modules.transaction.disbursements.DisbursementsApiException;
+import org.modules.transaction.receipts.CashReceiptApi;
+import org.modules.transaction.receipts.CashReceiptApiException;
+import org.modules.transaction.receipts.CashReceiptApiFactory;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.rmt2.api.handler.BaseAccountingMessageHandlerTest;
+import org.rmt2.api.handler.HandlerCacheMockData;
+import org.rmt2.api.handlers.transaction.cashdisbursement.CashDisbursementApiHandler;
+import org.rmt2.api.handlers.transaction.receipts.CashReceiptsApiHandler;
+import org.rmt2.constants.ApiTransactionCodes;
+import org.rmt2.constants.MessagingConstants;
+import org.rmt2.jaxb.AccountingTransactionResponse;
+import org.rmt2.jaxb.XactType;
+
+import com.api.config.SystemConfigurator;
+import com.api.messaging.handler.MessageHandlerCommandException;
+import com.api.messaging.handler.MessageHandlerResults;
+import com.api.persistence.AbstractDaoClientImpl;
+import com.api.persistence.db.orm.Rmt2OrmClientFactory;
+import com.api.util.RMT2File;
+
+/**
+ * Tests the creation and reversal of cash receipts API message handler
+ * 
+ * @author roy.terrell
+ *
+ */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ AbstractDaoClientImpl.class, Rmt2OrmClientFactory.class,
+    CashReceiptsApiHandler.class, CashReceiptApiFactory.class, SystemConfigurator.class })
+public class CashReceiptsMessageHandlerTest extends BaseAccountingMessageHandlerTest {
+
+    private CashReceiptApi mockApi;
+
+    /**
+     * 
+     */
+    public CashReceiptsMessageHandlerTest() {
+        return;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see testcases.messaging.MessageToListenerToHandlerTest#setUp()
+     */
+    @Before
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        mockApi = Mockito.mock(CashReceiptApi.class);
+        PowerMockito.mockStatic(CashReceiptApiFactory.class);
+        PowerMockito.when(CashReceiptApiFactory.createApi()).thenReturn(this.mockApi);
+        doNothing().when(this.mockApi).close();
+        
+        List<XactTypeDto> mockXactTypeListData = HandlerCacheMockData.createMockXactTypes();
+        try {
+            when(this.mockApi.getXactTypes(null)).thenReturn(mockXactTypeListData);
+        } catch (XactApiException e) {
+            Assert.fail("Unable to setup mock stub for fetching a cash receipt treansaction type data");
+        }
+        
+        return;
+    }
+
+
+    
+    /*
+     * (non-Javadoc)
+     * 
+     * @see testcases.messaging.MessageToListenerToHandlerTest#tearDown()
+     */
+    @After
+    public void tearDown() throws Exception {
+        return;
+    }
+
+    @Test
+    public void testSuccess_Fetch_BasicCriteria_Header() {
+        String request = RMT2File.getFileContentsAsString(
+                "xml/transaction/disbursements/cash/CashDisbursementBasicQueryRequestHeader.xml");
+        List<XactDto> mockListSingleData = CashReceiptsMockData.createMockSingleTransaction();
+        List<XactDto> mockListData = CashReceiptsMockData.createMockTransactions();
+
+
+        try {
+            when(this.mockApi.getXact(isA(XactDto.class))).thenReturn(mockListData);
+        } catch (XactApiException e) {
+            Assert.fail("Unable to setup mock stub for fetching a cash receipt transaction");
+        }
+        
+        MessageHandlerResults results = null;
+        CashReceiptsApiHandler handler = new CashReceiptsApiHandler();
+        try {
+            results = handler.processMessage(ApiTransactionCodes.ACCOUNTING_CASHDISBURSE_GET, request);
+        } catch (MessageHandlerCommandException e) {
+            e.printStackTrace();
+            Assert.fail("An unexpected exception was thrown");
+        }
+        Assert.assertNotNull(results);
+        Assert.assertNotNull(results.getPayload());
+
+        AccountingTransactionResponse actualRepsonse = 
+                (AccountingTransactionResponse) jaxb.unMarshalMessage(results.getPayload().toString());
+        Assert.assertEquals(1, actualRepsonse.getProfile().getTransactions().getTransaction().size());
+        Assert.assertEquals(1, actualRepsonse.getReplyStatus().getRecordCount().intValue());
+        Assert.assertEquals(MessagingConstants.RETURN_CODE_SUCCESS, actualRepsonse.getReplyStatus().getReturnCode().intValue());
+        Assert.assertEquals(MessagingConstants.RETURN_STATUS_SUCCESS, actualRepsonse.getReplyStatus().getReturnStatus());
+        Assert.assertEquals("Cash disbursement record(s) found", actualRepsonse.getReplyStatus().getMessage());
+        
+        Assert.assertNotNull(actualRepsonse.getProfile());
+        Assert.assertNotNull(actualRepsonse.getProfile().getTransactions());
+        Assert.assertTrue(actualRepsonse.getProfile().getTransactions().getTransaction().size() > 0);
+        for (int ndx = 0; ndx < actualRepsonse.getProfile().getTransactions().getTransaction().size(); ndx++) {
+            XactType a = actualRepsonse.getProfile().getTransactions().getTransaction().get(ndx);
+            Assert.assertNotNull(a.getXactId());
+            Assert.assertEquals(111111, a.getXactId().intValue());
+        }
+    }
+    
+  
+    @Test
+    public void testError_Update_API_Error() {
+        String request = RMT2File.getFileContentsAsString(
+                "xml/transaction/disbursements/cash/CashDisbursementBasicQueryRequestHeader.xml");
+        List<XactDto> mockListData = CashReceiptsMockData.createMockSingleTransaction();
+
+        try {
+            when(this.mockApi.receivePayment(isA(XactDto.class), isA(Integer.class)))
+                 .thenThrow(new DisbursementsApiException("An Xact API test error occurred"));
+        } catch (CashReceiptApiException e) {
+            Assert.fail("Unable to setup mock stub for receive cash payment transaction");
+        }
+        
+        MessageHandlerResults results = null;
+        CashReceiptsApiHandler handler = new CashReceiptsApiHandler();
+        try {
+            results = handler.processMessage(ApiTransactionCodes.ACCOUNTING_CASHRECEIPT_CREATE, request);
+        } catch (MessageHandlerCommandException e) {
+            e.printStackTrace();
+            Assert.fail("An unexpected exception was thrown");
+        }
+        
+        Assert.assertNotNull(results);
+        Assert.assertNotNull(results.getPayload());
+
+        AccountingTransactionResponse actualRepsonse = 
+                (AccountingTransactionResponse) jaxb.unMarshalMessage(results.getPayload().toString());
+        Assert.assertNull(actualRepsonse.getProfile());
+        Assert.assertEquals(MessagingConstants.RETURN_STATUS_SUCCESS, actualRepsonse.getReplyStatus().getReturnStatus());
+        Assert.assertEquals(-1, actualRepsonse.getReplyStatus().getReturnCode().intValue());
+        Assert.assertEquals(CashDisbursementApiHandler.MSG_FAILURE, actualRepsonse.getReplyStatus().getMessage());
+        Assert.assertEquals("An Xact API test error occurred",
+                actualRepsonse.getReplyStatus().getExtMessage());
+    }
+    
+
+    @Test
+    public void testError_Incorrect_Trans_Code() {
+        String request = RMT2File.getFileContentsAsString(
+                "xml/transaction/common/TransactionQueryInvalidTranCodeRequest.xml");
+        
+        MessageHandlerResults results = null;
+        CashDisbursementApiHandler handler = new CashDisbursementApiHandler();
+        try {
+            results = handler.processMessage("INCORRECT_TRAN_CODE", request);
+        } catch (MessageHandlerCommandException e) {
+            e.printStackTrace();
+            Assert.fail("An unexpected exception was thrown");
+        }
+        
+        Assert.assertNotNull(results);
+        Assert.assertNotNull(results.getPayload());
+
+        AccountingTransactionResponse actualRepsonse = 
+                (AccountingTransactionResponse) jaxb.unMarshalMessage(results.getPayload().toString());
+        Assert.assertNull(actualRepsonse.getProfile());
+        Assert.assertEquals(MessagingConstants.RETURN_STATUS_BAD_REQUEST, actualRepsonse.getReplyStatus().getReturnStatus());
+        Assert.assertEquals(-1, actualRepsonse.getReplyStatus().getReturnCode().intValue());
+        Assert.assertEquals(CashDisbursementApiHandler.ERROR_MSG_TRANS_NOT_FOUND + "INCORRECT_TRAN_CODE", actualRepsonse
+                .getReplyStatus().getMessage());
+    }
+
+}
