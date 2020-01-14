ef98563e8114642327658576335c6ad7efac8b57
Terrell <roy.terrell@accenture.com>
2020-01-13 23:07:28 -0600

    IS-28: Moved constant variables from sales order specific class.

--- amh-parent/amh_accounting/src/main/java/org/ApiMessageHandlerConst.java
+++ amh-parent/amh_accounting/src/main/java/org/ApiMessageHandlerConst.java
@@ -1,7 +1,7 @@
 package org;
 
 /**
- * Common API message handler constants
+ * Common message handler API constants
  * 
  * @author appdev
  *
@@ -11,6 +11,11 @@ public class ApiMessageHandlerConst {
     public static final String TARGET_LEVEL_HEADER = "HEADER";
     public static final String TARGET_LEVEL_DETAILS = "DETAILS";
     public static final String TARGET_LEVEL_FULL = "FULL";
+    public static final String MSG_DETAILS_NOT_SUPPORTED = "Transaction level \"DETAILS\" is not supported at this time";
+    public static final String MSG_MISSING_TARGET_LEVEL = "Fetch request must contain a target level value";
+    public static final String MSG_INCORRECT_TARGET_LEVEL = "Fetch request contains an invalid target level: %s";
+    public static final String MSG_MISSING_PROFILE_DATA = "Profile data is required for create operation";
+    public static final String MSG_MISSING_TRANSACTION_SECTION = "Transaction section is missing from the profile data section";
 
     /**
      * 
