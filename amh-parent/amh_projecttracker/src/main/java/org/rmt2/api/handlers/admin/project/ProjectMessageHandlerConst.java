8ec6ce1166acf0b812d5479e4e00bea60a409ef9
appdev <royterrell2@gmail.com>
2020-05-19 05:43:07 -0500

    IS-42:  Added logic to look for deither the client id aor the business id when creating a project.   Added validation logic to PRojectUpdateApiHandler to check if the business id exists as a business contact in the addressbook project and a customer in the accounting project.   Added addressbook and accounting projects to the .pom file for all of the above to work properly.

--- amh-parent/amh_projecttracker/src/main/java/org/rmt2/api/handlers/admin/project/ProjectMessageHandlerConst.java
+++ amh-parent/amh_projecttracker/src/main/java/org/rmt2/api/handlers/admin/project/ProjectMessageHandlerConst.java
@@ -13,10 +13,14 @@ public class ProjectMessageHandlerConst {
     public static final String MESSAGE_NEW_PROJECT_UPDATE_SUCCESS = "Project was created successfully";
     public static final String MESSAGE_EXISTING_PROJECT_UPDATE_SUCCESS = "Project was modified successfully";
     public static final String MESSAGE_NEW_PROJECT_UPDATE_FAILED = "Error occurred creating new project";
+    public static final String MESSAGE_ERROR_CREATING_PROJECT_CLIENT = "Unable to create project client for new project";
     public static final String MESSAGE_EXISTING_PROJECT_UPDATE_FAILED = "Error occurred modifying project";
     public static final String MESSAGE_PROJECT_DELETE_SUCCESS = "Project(s) were deleted successfully";
     public static final String MESSAGE_PROJECT_DELETE_FAILED = "Error occurred deleting project(s)";
     public static final String VALIDATION_PROJECT_MISSING = "Update/Delete operations require one project";
+    public static final String VALIDATION_BUSID_MISSING_FOR_NEW_PROJECT = "New project request must contain the customer's business id when the client id is not provided";
+    public static final String VALIDATION_BUSID_NOT_BUSINESS_CONTACT = "Request for new project requires a business id that exists in the addressbook system as a business contact";
+    public static final String VALIDATION_BUSID_NOT_CUSTOMER = "Request for new project requires a business id that exists in the acounting system as a customer";
 
     /**
      * 
