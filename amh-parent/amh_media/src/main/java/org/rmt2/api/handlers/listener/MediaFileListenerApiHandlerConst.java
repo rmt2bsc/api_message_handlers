f5623a397938bfec41c6ad0a54954ff4f1d06fe9
appdev <royterrell2@gmail.com>
2021-01-23 23:58:21 -0600

    IS-52:  Developed message handler for transaction code, STOP_MEDIA_FILE_LISTENER

--- amh-parent/amh_media/src/main/java/org/rmt2/api/handlers/listener/MediaFileListenerApiHandlerConst.java
+++ amh-parent/amh_media/src/main/java/org/rmt2/api/handlers/listener/MediaFileListenerApiHandlerConst.java
@@ -13,7 +13,7 @@ public class MediaFileListenerApiHandlerConst {
     public static final String MESSAGE_STOPPED = "Media file listener stopped";
     public static final String MESSAGE_STATUS = "Media file listener status: " + ApiMessageHandlerConst.MSG_PLACEHOLDER;
 
-    public static final String MESSAGE_ERROR = "Media file listener error";
+    public static final String MESSAGE_ERROR = "Media file listener error.  Contact the system adminstrator";
 
     public MediaFileListenerApiHandlerConst() {
 
