e127ccef53fa3efa0abf5dfb04c41f5ab27f74ad
DIR\roy.terrell <rmt2bsc@gmail.com>
2021-08-19 12:55:48 -0500

    IS-63:  Removed param entry fron default constructor's javadoc

--- amh-parent/amh_security/src/main/java/org/rmt2/api/handlers/auth/UserLogoutApiHandler.java
+++ amh-parent/amh_security/src/main/java/org/rmt2/api/handlers/auth/UserLogoutApiHandler.java
@@ -31,7 +31,6 @@ public class UserLogoutApiHandler extends UserAuthenticationApiHandler {
     /**
      * Default constructor
      * 
-     * @param payload
      */
     public UserLogoutApiHandler() {
         super();
